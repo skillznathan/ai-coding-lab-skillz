2f24871e593a4ce161b68868ab7fad5bd232c0b9
Skillz Nwauzor <30082157@cc.peralta.edu>
2026-05-04 19:27:02 +0000

    Implement addGrade

---
+++ GradeBook.cpp
@@ -0,0 +1,17 @@
+#include "GradeBook.h"
+
+GradeBook::GradeBook() {}
+
+grades_.push_back({assignment, score});
+
+double GradeBook::getAverage() const {
+    return 0.0;
+}
+
+double GradeBook::getHighest() const {
+    return 0.0;
+}
+
+int GradeBook::numGrades() const {
+    return 0;
+}
